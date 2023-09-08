836b763888cdd023ef9956cb6d1a102bde781432
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-08 08:35:47 +0200

    setting seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([UserSeeder::class, TypeSeeder::class, ProjectSeeder::class]);
+        $this->call([UserSeeder::class, TechnologySeeder::class, TypeSeeder::class, ProjectSeeder::class]);
     }
 }
--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,21 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = [
+            ['label' => 'HTML', 'color' => '#F1652A'],
+            ['label' => 'CSS', 'color' => '#2965F1'],
+            ['label' => 'ES6', 'color' => '#F7DF1E'],
+            ['label' => 'Bootstrap', 'color' => '#7952B3'],
+            ['label' => 'PHP', 'color' => '#4F5B93'],
+            ['label' => 'SQL', 'color' => '#E38A00'],
+            ['label' => 'Laravel', 'color' => '#FF2D20'],
+            ['label' => 'VueJS', 'color' => '#4FC08D'],
+        ];
+        foreach($technologies as $technology) {
+            $new_technology = new Technology();
+            $new_technology->label = $technology['label'];
+            $new_technology->color = $technology['color'];
+            $new_technology->save();
+        }
     }
 }
